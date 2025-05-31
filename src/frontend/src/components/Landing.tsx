4cdee7ad2dcaeeadf27dab57f85b996a48ada568
Paweł Lidwin <lidwinpawel@gmail.com>
2025-05-31 16:45:18 +0200

    add chart and new layout

--- src/frontend/src/components/Landing.tsx
+++ src/frontend/src/components/Landing.tsx
@@ -1,73 +1,96 @@
-import React from 'react';
-import Navbar from '@components/layout/Navbar';
-import SectionDivider from '@components/layout/SectionDivider';
-import Footer from '@components/layout/Footer';
-import Button from '@components/Button';
-import Form from '@components/Form';
+import React from "react";
+import Navbar from "@components/layout/Navbar";
+import SectionDivider from "@components/layout/SectionDivider";
+import Footer from "@components/layout/Footer";
+import Button from "@components/Button";
+import Form from "@components/Form";
+import ResultCard from "@components/ResultCard";
+import CostsChart from "./CostsChart";
 
 const Landing: React.FC = () => {
-    const fields = [
-      { name: 'email', label: 'Email', type: 'email', placeholder: 'wpisz@email', required: true },
-      { name: 'password', label: 'Hasło', type: 'password', placeholder: 'wpisz hasło', required: true },
-    ];
+  const fields = [
+    {
+      name: "email",
+      label: "Email",
+      type: "email",
+      placeholder: "wpisz@email",
+      required: true,
+    },
+    {
+      name: "password",
+      label: "Hasło",
+      type: "password",
+      placeholder: "wpisz hasło",
+      required: true,
+    },
+  ];
 
-    const handleFormSubmit = (data: { [key: string]: string }) => {
-      console.log('Formularz wysłany:', data);
-      // Tutaj możesz dodać dowolną logikę, np. wysłanie danych na serwer
-    };
+  const handleFormSubmit = (data: { [key: string]: string }) => {
+    console.log("Formularz wysłany:", data);
+    // Tutaj możesz dodać dowolną logikę, np. wysłanie danych na serwer
+  };
 
-    return (
-        <div>
-            {/* 1. Navbar */}
-            <Navbar />
+  return (
+    <div className="flex flex-col items-center">
+      {/* 1. Navbar */}
+      <Navbar />
 
-            {/* 2. Pierwsza sekcja */}
-            <SectionDivider id="Formularz">
-
-                {/*
+      {/* 2. Pierwsza sekcja */}
+      <SectionDivider id="Formularz">
+        {/*
 
                 Ciekawostka lewy górny, formularz na środku, ładny obrazek jakiś gradient na tło
                 guzik na dół prowadzący
 
 
                 */}
-                <p className="center">Wypełnij poniższy formularz, aby się zarejestrować na wydarzenie!</p>
-                <h2 className={'center'}>Zajebisty formularz</h2>
-                <Form
-                  fields={fields}
-                  onSubmit={handleFormSubmit}
-                  title="Zarejestruj się"
-                  width="100%"
-                  className="centerItem"
-                />
-                <Button className={'centerItem'}>Zobacz stonks</Button>
-            </SectionDivider>
+        <p className="center">
+          Wypełnij poniższy formularz, aby się zarejestrować na wydarzenie!
+        </p>
+        <h2 className={"center"}>Zajebisty formularz</h2>
+        <Form
+          fields={fields}
+          onSubmit={handleFormSubmit}
+          title="Zarejestruj się"
+          width="100%"
+          className="centerItem"
+        />
+        <Button className={"centerItem"}>Zobacz stonks</Button>
+      </SectionDivider>
 
-            {/* 3. Druga sekcja */}
-            <SectionDivider id="sekcja2">
-                <h2>Sekcja 2</h2>
-                <p>Kolejna porcja treści, którą możesz sobie edytować według uznania.</p>
-            </SectionDivider>
-
-            {/* 4. Trzecia sekcja z headerem i guzikiem na środku */}
-            <SectionDivider id="sekcja3">
-                <h2 className="section-heading-center">Trzecia Sekcja</h2>
-                <div className="section-flex-center">
-                    <Button
-                      onClick={() => {
-                        // tutaj możesz wstawić dowolną akcję po kliknięciu
-                        alert('Kliknięto guzik w trzeciej sekcji!');
-                      }}
-                    >
-                      Kliknij mnie
-                    </Button>
-                </div>
-            </SectionDivider>
-
-            {/* 5. Footer */}
-            <Footer />
+      {/* 3. Druga sekcja */}
+      <SectionDivider id="sekcja2">
+        <h2>Sekcja 2</h2>
+        <p>
+          Kolejna porcja treści, którą możesz sobie edytować według uznania.
+        </p>
+        <div className="flex flex-row gap-8">
+          <ResultCard title="Po ilu latach inwestycja się zwróci?" value="2" />
+          <ResultCard title="Zestawienie">
+            <CostsChart />
+          </ResultCard>
         </div>
-    );
+      </SectionDivider>
+
+      {/* 4. Trzecia sekcja z headerem i guzikiem na środku */}
+      <SectionDivider id="sekcja3">
+        <h2 className="section-heading-center">Trzecia Sekcja</h2>
+        <div className="section-flex-center">
+          <Button
+            onClick={() => {
+              // tutaj możesz wstawić dowolną akcję po kliknięciu
+              alert("Kliknięto guzik w trzeciej sekcji!");
+            }}
+          >
+            Kliknij mnie
+          </Button>
+        </div>
+      </SectionDivider>
+
+      {/* 5. Footer */}
+      <Footer />
+    </div>
+  );
 };
 
 export default Landing;
--- src/frontend/src/components/ResultCard.tsx
+++ src/frontend/src/components/ResultCard.tsx
@@ -1,7 +1,25 @@
+import React from "react";
 
-export function ResultCard() {
-    
-    return <div className="result-card">
-                
-    </div>;
+interface ResultCardProps {
+  title?: string;
+  value?: string;
+  children?: React.ReactNode;
 }
+
+const ResultCard: React.FC<ResultCardProps> = ({ title, value, children }) => {
+  return (
+    <div>
+      {title && <h3 className="font-semibold text-xl my-6">{title}</h3>}
+      <div className="aspect-square size-100 bg-white shadow-gray-300 shadow-md rounded-xl p-8 border-green-400 border flex flex-col">
+        {value && (
+          <div className="text-[12rem] font-bold w-full h-full text-center text-green-500 flex items-center justify-center">
+            <p>{value}</p>
+          </div>
+        )}
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default ResultCard;
