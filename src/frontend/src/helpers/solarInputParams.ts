f900df4be1d5637e3f900c47d989a3530c8ebbef
Jan Pasławski <jpaslawski93@gmail.com>
2025-05-31 15:38:58 +0200

    service and helpers added

---
+++ src/frontend/src/helpers/solarInputParams.ts
@@ -0,0 +1,10 @@
+export let defaultSolarParams = {
+    yearlyConsumptionWithoutPV: 4000,
+    initialEnergyPurchasePrice: 1.0,
+    pvSystemSizeKw: 5,
+    annualEnergyProduced: 5000,
+    initialEnergySellPrice: 0.5,
+    pvCostPerKw: 5000,
+    selfConsumptionRate: 25,
+    annualEnergyPriceGrowth: 7
+}
