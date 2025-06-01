5724e285f3c99742c3dd278845a88af8a914f6cf
Jan Pasławski <jpaslawski93@gmail.com>
2025-06-01 05:02:44 +0200

    csv render statically

--- src/server/GoSolar.API/Controllers/DetailsController.cs
+++ src/server/GoSolar.API/Controllers/DetailsController.cs
@@ -8,19 +8,21 @@ namespace GoSolar.API.Controllers
     public class DetailsController : ControllerBase
     {
         [HttpGet]
-        public ActionResult GetDetailedData()
+        public JsonResult GetDetailedData()
         {
-            var consumptionTotal = new List<string[]>();
+            var consumptionTotalchart1 = new List<string[]>();
+            var consumptionTotalchart2 = new List<string[]>();
+            var consumptionTotalchart3 = new List<string[]>();
 
-            var path1 = Path.Combine(Directory.GetCurrentDirectory(), "CSVs/calkowita_konsumpcja_dla_polski.csv"
-                ); 
-            using (TextFieldParser parser = new TextFieldParser(path: path1))
+            var path = Path.Combine(Directory.GetCurrentDirectory(), 
+                "CSVs/calkowita_konsumpcja_dla_polski.csv"); 
+            using (TextFieldParser parser = new TextFieldParser(path: path))
             {
                 parser.TextFieldType = FieldType.Delimited;
                 parser.SetDelimiters(",");
                 var i = 0;
 
-                while (!parser.EndOfData || i > 5000)
+                while (!parser.EndOfData)
                 {
                     //Process row
                     string[] fields = parser.ReadFields()!;
@@ -29,15 +31,61 @@ namespace GoSolar.API.Controllers
                         i++;
                         continue;
                     }
-                    consumptionTotal.Add(fields);
-                    //foreach (string field in fields)
-                    //{
-                    //    //
-                    //}
+                    consumptionTotalchart1.Add(fields);
+                    if (i == 2000) break;
                 }
             }
 
-            return Ok(consumptionTotal);
+            path = Path.Combine(Directory.GetCurrentDirectory(), 
+                "CSVs/calkowita_cena_sprzedazy.csv");
+            using (TextFieldParser parser = new TextFieldParser(path: path))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+                var i = 0;
+
+                while (!parser.EndOfData)
+                {
+                    //Process row
+                    string[] fields = parser.ReadFields()!;
+                    if (i == 0)
+                    {
+                        i++;
+                        continue;
+                    }
+                    consumptionTotalchart2.Add(fields);
+                    if (i == 2000) break;
+
+                }
+            }
+            path = Path.Combine(Directory.GetCurrentDirectory(), 
+                "CSVs/produkcja_energii.csv");
+            using (TextFieldParser parser = new TextFieldParser(path: path))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+                var i = 0;
+
+                while (!parser.EndOfData)
+                {
+                    //Process row
+                    string[] fields = parser.ReadFields()!;
+                    if (i == 0)
+                    {
+                        i++;
+                        continue;
+                    }
+                    consumptionTotalchart3.Add(fields);
+                    if (i == 2000) break;
+
+                }
+            }
+            var result = new List<List<string[]>>();
+            result.Add(consumptionTotalchart1);
+            result.Add(consumptionTotalchart2);
+            result.Add(consumptionTotalchart3);
+
+            return new JsonResult(result);
         }
 
     }
