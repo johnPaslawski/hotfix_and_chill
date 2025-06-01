42831bc827135cf2f72958d1bb1d1606967f0521
Paweł Lidwin <lidwinpawel@gmail.com>
2025-06-01 02:49:28 +0200

    feat: efficiency charts

---
+++ src/frontend/src/components/charts/EfficiencyChart.tsx
@@ -0,0 +1,58 @@
+import type { SavingsRecord } from "@/services/CalculatorService";
+import {
+  ResponsiveContainer,
+  BarChart,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Bar,
+  CartesianGrid,
+} from "recharts";
+
+interface EfficiencyChart {
+  savings: SavingsRecord[];
+}
+
+const EfficiencyChart: React.FC<EfficiencyChart> = ({ savings }) => {
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <BarChart data={savings} margin={{ bottom: 10, left: 40 }}>
+        <defs>
+          <linearGradient id="colorSum" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor="#00c853" stopOpacity={0.8} />
+            <stop offset="95%" stopColor="#00c853" stopOpacity={0} />
+          </linearGradient>
+        </defs>
+        <XAxis
+          dataKey={"year"}
+          label={{ value: 'Rok', position: 'insideBottom', offset: -5 }}
+          interval={2}
+          tick={{ fontSize: 12, fill: '#555' }}
+          axisLine={{ stroke: '#ccc' }}
+          tickLine={{ stroke: '#ccc' }}
+        />
+        <YAxis
+          label={{ value: 'Efektywność', angle: -90, position: 'insideLeft', offset: -20 }}
+          tick={{ fontSize: 12, fill: '#555' }}
+          axisLine={{ stroke: '#ccc' }}
+          tickLine={{ stroke: '#ccc' }}
+        />
+        <Tooltip
+          formatter={(value: number) => `${value.toFixed(2)}`}
+          contentStyle={{ backgroundColor: '#222', border: 'none', borderRadius: 4, color: '#fff' }}
+          labelStyle={{ color: '#aaa' }}
+        />
+        <CartesianGrid strokeDasharray="3 3" />
+
+        <Bar
+          dataKey="sum"
+          fill="url(#colorSum)"
+          radius={[10, 10, 0, 0]}
+          isAnimationActive={false}
+        />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default EfficiencyChart;
--- src/frontend/src/services/CalculatorService.ts
+++ src/frontend/src/services/CalculatorService.ts
@@ -1,67 +1,96 @@
-import "../helpers/solarInputParams"
-import { defaultSolarParams as dsp }  from "../helpers/solarInputParams"
+import "../helpers/solarInputParams";
+import { defaultSolarParams as dsp } from "../helpers/solarInputParams";
 
 export interface SavingsRecord {
-    year: number,
-    sum: number
+  year: number;
+  sum: number;
 }
 
 export interface SummaryRecord {
-    year: number,
-    with: number
-    without: number,
+  year: number;
+  with: number;
+  without: number;
 }
 
 export interface ChartRecords {
-    yearsToReturn: number,
-    summaryRecords: SummaryRecord[],
-    savingsRecords: SavingsRecord[]
+  yearsToReturn: number;
+  summaryRecords: SummaryRecord[];
+  savingsRecords: SavingsRecord[];
 }
 
-export function CalculateSavings(_params: typeof dsp, numOfYears: number): ChartRecords {
+export function CalculateSavings(
+  _params: typeof dsp,
+  numOfYears: number
+): ChartRecords {
+  let thisYearEnergyPurchaseCost = _params.initialEnergyPurchasePrice / 1.07;
+  let thisYearEnergySellingPrice = 1;
+  let cumulativeSavings = -dsp.pvCostPerKw;
+  const chartRecords = {} as ChartRecords;
+  const summaryRecordChart = [] as SummaryRecord[];
+  const savingsRecords = [] as SavingsRecord[];
+  let yearsProfitNum: number = 0;
 
-    let thisYearEnergyPurchaseCost = _params.initialEnergyPurchasePrice / 1.07
-    let thisYearEnergySellingPrice = 1
-    let cumulativeSavings = - dsp.pvCostPerKw
-    const chartRecords = {} as ChartRecords;
-    const summaryRecordChart = [] as SummaryRecord[];
-    const savingsRecords = [] as SavingsRecord[];
-    let yearsProfitNum: number = 0;
+  for (let index = 1; index <= numOfYears; index++) {
+    const summaryRecord = {} as SummaryRecord;
+    const savingsRecord = {} as SavingsRecord;
 
-    for (let index = 1; index <= numOfYears; index++) {
-        const summaryRecord = {} as SummaryRecord;
-        const savingsRecord = {} as SavingsRecord;
+    thisYearEnergyPurchaseCost =
+      thisYearEnergyPurchaseCost * (1 + _params.annualEnergyPriceGrowth / 100);
+    thisYearEnergySellingPrice =
+      index === 1
+        ? 0
+        : thisYearEnergySellingPrice *
+          (1 + _params.annualEnergyPriceGrowth / 100);
+    const energyConstWithoutPV =
+      thisYearEnergyPurchaseCost * _params.yearlyConsumptionWithoutPV;
+    const selfConsumption =
+      (_params.selfConsumptionRate / 100) * _params.annualEnergyProduced;
+    const energyGivenBack = _params.annualEnergyProduced - selfConsumption;
+    const energyConsumptionWithPV =
+      _params.yearlyConsumptionWithoutPV - selfConsumption;
+    const thisYearProfit = thisYearEnergySellingPrice * energyGivenBack;
+    const energyCostWithPV =
+      thisYearEnergyPurchaseCost * energyConsumptionWithPV - thisYearProfit;
+    const annualSavings = energyConstWithoutPV - energyCostWithPV;
+    cumulativeSavings = cumulativeSavings + annualSavings;
 
-        thisYearEnergyPurchaseCost = thisYearEnergyPurchaseCost * (1 + _params.annualEnergyPriceGrowth / 100)
-        thisYearEnergySellingPrice = index === 1 
-            ? 0
-            : thisYearEnergySellingPrice * (1 + _params.annualEnergyPriceGrowth / 100)
-        const energyConstWithoutPV = thisYearEnergyPurchaseCost * _params.yearlyConsumptionWithoutPV
-        const selfConsumption = (_params.selfConsumptionRate / 100) * _params.annualEnergyProduced
-        const energyGivenBack = _params.annualEnergyProduced - selfConsumption
-        const energyConsumptionWithPV = _params.yearlyConsumptionWithoutPV - selfConsumption
-        const thisYearProfit = thisYearEnergySellingPrice * energyGivenBack
-        const energyCostWithPV = thisYearEnergyPurchaseCost * energyConsumptionWithPV - thisYearProfit
-        const annualSavings = energyConstWithoutPV - energyCostWithPV
-        cumulativeSavings = cumulativeSavings + annualSavings 
+    summaryRecord.year = index;
+    summaryRecord.without = energyConstWithoutPV;
+    summaryRecord.with = energyCostWithPV;
+    summaryRecordChart.push(summaryRecord);
 
-        summaryRecord.year = index;
-        summaryRecord.without = energyConstWithoutPV;
-        summaryRecord.with = energyCostWithPV;
-        summaryRecordChart.push(summaryRecord);
+    savingsRecord.year = index;
+    savingsRecord.sum = cumulativeSavings;
+    savingsRecords.push(savingsRecord);
 
-        savingsRecord.year = index;
-        savingsRecord.sum = cumulativeSavings;
-        savingsRecords.push(savingsRecord);
-        
-        if (!yearsProfitNum && cumulativeSavings > 0) {
-            yearsProfitNum = index;
-        }  
+    if (!yearsProfitNum && cumulativeSavings > 0) {
+      yearsProfitNum = index;
     }
+  }
 
-    chartRecords.summaryRecords = summaryRecordChart;
-    chartRecords.savingsRecords = savingsRecords;
-    chartRecords.yearsToReturn = yearsProfitNum;
-    
-    return chartRecords;
+  chartRecords.summaryRecords = summaryRecordChart;
+  chartRecords.savingsRecords = savingsRecords;
+  chartRecords.yearsToReturn = yearsProfitNum;
+
+  return chartRecords;
+}
+
+export function CalculateEfficiency(numOfYears: number): SavingsRecord[] {
+  const delta = 0.5 / 100;
+  let efficiency = 100;
+  const efficiencyChart = [
+    {
+      year: 1,
+      sum: efficiency,
+    },
+  ];
+  for (let i = 2; i <= numOfYears; i++) {
+    efficiency = efficiency - delta * efficiency;
+    efficiencyChart.push({
+      year: i,
+      sum: efficiency,
+    });
+  }
+
+  return efficiencyChart;
 }
